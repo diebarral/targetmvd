caa3166654e2834532ce02ad5e364ec0c83c30fc
Diego Barral <diego@rootstrap.com>
2017-05-19 15:46:17 -0300

    Added rspec tests for Target model

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,46 +1,44 @@
 class Target < ApplicationRecord
   acts_as_mappable :default_units => :kms, :lat_column_name => :latitude, :lng_column_name => :longitude
   belongs_to :user
-  has_one :topic
+  belongs_to :topic
 
   after_commit :search_for_compatible_targets, on: [:create, :update]
-
-  private
 
   def search_for_compatible_targets
 
     origin = Geokit::LatLng.new(self.latitude, self.longitude)
-    compatibleTargets = Target.within(self.radius * 0.001, :units => :kms, :origin => origin).all.select { |t| t.user_id != self.user_id && t.topic_id == self.topic_id }
+    compatible_targets = Target.within(self.radius * 0.001, :units => :kms, :origin => origin).all.select { |t| t.user_id != self.user_id && t.topic_id == self.topic_id }
 
-    ownersOfCompatibleTargets = Array.new
+    owners_of_compatible_targets = Array.new
 
-    compatibleTargets.each do |t|
+    compatible_targets.each do |t|
 
       # TO DO: create Match object in database, after checking if it doesn´t exist already.
       # If it exists, a notification shouldn´t be sent.
 
       username = t.user.name
 
-      if not ownersOfCompatibleTargets.include? username
-        ownersOfCompatibleTargets.push(username)
+      if not owners_of_compatible_targets.include? username
+        owners_of_compatible_targets.push(username)
         create_notification([self.user.name], t.user_id)
       end
 
     end
 
-    create_notification(ownersOfCompatibleTargets, self.user_id)
-
+    if not owners_of_compatible_targets.empty?
+      create_notification(owners_of_compatible_targets, self.user_id)
+    end
   end
 
-  def create_notification(ownersOfCompatibleTargets, recipient)
+  def create_notification(owners_of_compatible_targets, recipient)
 
     message = 'Start a conversation now with '
 
-    ownersOfCompatibleTargets.each_with_index do |username, i|
-
+    owners_of_compatible_targets.each_with_index do |username, i|
       if i == 0
           message << username
-      elsif i < ownersOfCompatibleTargets.length - 1
+      elsif i < owners_of_compatible_targets.length - 1
           message << ', ' << username
       else
           message << ' and ' << username
